0270d09ece2f0c1a806315543d998baf39e332a8
Iyanubello <belloiyanuolaoluwait@gmail.com>
2022-04-27 16:37:56 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
-nclude "main.h"
+#include "main.h"
 /**
- *  * _strchr - Entry point
- *   * @s: input
- *    * @c: input
- *     * Return: Always 0 (Success)
- *      */
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
 char *_strchr(char *s, char c)
 {
 int i = 0;
@@ -12,7 +12,7 @@ int i = 0;
 for (; s[i] >= '\0'; i++)
 {
 if (s[i] == c)
-return (&s[i]);
+return (&s[ki]);
 }
 return (0);
 }
